faff6d1f3cdbf01c92848a20d1c606628feccf4c
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2022-02-20 14:23:23 +0800

    added the error handiling to the middleware and updated the delete request to work with the database

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const morgan = require("morgan")
 const cors = require('cors')
 const Person = require('./models/person')
 
 morgan.token("body", (request) => JSON.stringify(request.body))
 
+
+const app = express()
+
+
+
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 app.use(cors())
-app.use(express.static('build'))
+
+
+
 
 // let persons = [
 //     { 
@@ -46,14 +53,15 @@ app.get('/info', (request, response) => {
   })
   
 
-  app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
       response.json(persons)
       console.log(persons)
     })
   })
 
-  app.get('/api/persons/:id', (request, response) => {
+
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
       .then(person => {
         if (person) {
@@ -65,19 +73,15 @@ app.get('/info', (request, response) => {
       .catch(error => next(error))
   })
 
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     persons = persons.filter(person => person.id !== id)
-  
-//     response.status(204).end()
-//   })
 
-// const generateId = () => {
-//     const maxId = persons.length > 0
-//       ? Math.max(...persons.map(n => n.id))
-//       : 0
-//     return maxId + 1
-//   }
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
   
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -94,13 +98,6 @@ app.post('/api/persons', (request, response) => {
         })
       }
 
-    // const alreadyExists = persons.some(person => person.name.toLowerCase() === body.name.toLowerCase()) 
-    
-    // if (alreadyExists) {
-    //     return response.status(400).json({ 
-    //       error: 'name must be unique' 
-    //     })
-    //   }
   
     const person = new Person ({
       name: body.name,
@@ -112,6 +109,17 @@ app.post('/api/persons', (request, response) => {
       })
   })
 
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
+  app.use(errorHandler)
   
   const PORT = process.env.PORT
   app.listen(PORT, () => {
